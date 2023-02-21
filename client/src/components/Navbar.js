ff6df9caaecd297c0f1ed26f9e84d7144c80e471
Michael Hodges <Michael.Hodges1985@yahoo.com>
2023-02-20 21:11:57 -0600

    still working on the nav bar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,13 +2,21 @@ import React from "react";
 import logo from "../assets/ranch-digital-logo-1.png";
 
 const AppNavbar = (props) => {
-  // set modal display state
+  const navFunction = () => {
+    const x = document.getElementById("navigation-bar");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar"
+    }
+  }
 
   return (
     <>
-     <nav className="navbar">
+     <nav className="navbar" id="navigation-bar">
       <div className="nav-wrapper">
-        <ul className="right hide-on-med-and-down navbar-nav sidenav-trigger"> {props.children}</ul>
+        <ul className="right hide-on-med-and-down navbar-nav sidenav-trigger"> {props.children}
+        </ul>
       </div>
      </nav>
     </>

97dc8c95d13711153aceb589ceb99f239c20cf39
Michael Hodges <Michael.Hodges1985@yahoo.com>
2023-03-09 13:56:17 -0600

    added getreservation query and resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,64 @@
-
+const lodash = require("lodash");
 // The root provides a resolver function for each API endpoint
 // resolves the requests from the server.  
-const resolvers = {
-    hello: () => {
-      return 'Hello world!';
+const mockReservations = [
+{
+    _id: "jpiohjphioe",
+    dateBooked: "5-5-2023",
+    beginDate: "6-5-2023",
+    endDate: "7-5-2023",
+    downPaymentPaid: true,
+    downPaymentAmount: 200.00,
+    totalPrice: 400.00,
+    balance: 200.00,
+    paidInFull: false,
+    available: false,
+    reservedProperty: {
+        _id: "eeeeerrrrttt",
+        name: "The Lake House",
+        booked: true,
+        reserveCost: 400.00,
+        addressSt: "123 Main St.",
+        city: "Nashville",
+        state: "TN",
+        zip: "37090"
     },
-  };
+},
+{
+    _id: "rerererererer",
+    dateBooked: "4-5-2023",
+    beginDate: "5-5-2023",
+    endDate: "9-5-2023",
+    downPaymentPaid: true,
+    downPaymentAmount: 100.00,
+    totalPrice: 1000.00,
+    balance: 900.00,
+    paidInFull: false,
+    available: false,
+    reservedProperty: {
+        _id: "wwwwwwiiiiiii",
+        name: "The Jump House",
+        booked: true,
+        reserveCost: 1000.00,
+        addressSt: "400 Main St.",
+        city: "Nashville",
+        state: "TN",
+        zip: "37090"
+    },
+},
+]
+
+const resolvers = {
+    getReservations: () => {
+        return mockReservations;
+    },
+    getReservation: ({_id}) => {
+      const id = _id;
+      //if using database you would select from the DB by id.
+      const reservation = lodash.find(mockReservations, {_id: id});
+      return reservation;
+    } 
+}
 
 
   module.exports = resolvers
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,9 +1,45 @@
 const { buildSchema } = require('graphql');
 
-//defines the query types and datatypes.  
+//defines the data models, query arrangements and mutations.  
 const typeDefs = buildSchema(`
+  type Reservation {
+      _id: String
+      dateBooked: String
+      beginDate: String
+      endDate: String
+      downPaymentPaid: Boolean
+      downPaymentAmount: Int
+      totalPrice: Int
+      balance: Int
+      paidInFull: Boolean
+      available: Boolean
+      reservedProperty: Property
+  }
+
+  type Property {
+    _id: String
+    name: String
+    booked: Boolean
+    reserveCost: Int
+    addressSt: String
+    city: String
+    state: String
+    zip: String
+  }
+
+  type Customer {
+    _id: String
+    firstName: String
+    lastName: String
+    phone: String
+    email: String
+    password: String
+    reservations: [Reservation]
+  }
+
   type Query {
-    hello: String
+    getReservations: [Reservation],
+    getReservation(_id: String): Reservation
   }
 `);
 

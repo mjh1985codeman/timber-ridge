09067f669d45e327e4aa6cdd3a9936a618b8c449
Michael Hodges <Michael.Hodges1985@yahoo.com>
2023-04-29 10:43:56 -0500

    able to upload pics to s3 now

--- server/utils/s3.js
+++ server/utils/s3.js
@@ -13,15 +13,14 @@ const params = {
 const s3Client = new AWS.S3(params);
 
 const s3Actions = {
-    getURL: async (propId) => {
+    getURL: (propId) => {
         const fileName = `${propId}.json`
-        const s3bucketUrl = await s3Client.getSignedUrl("putObject", {
+        const s3bucketUrl = s3Client.getSignedUrl("putObject", {
             Bucket: params.BucketName,
             Key: fileName,
             Expires: 60,
             ContentType: 'application/json'
         });
-        console.log("s3bucketUrl: ", s3bucketUrl)
         return s3bucketUrl
     }
 };

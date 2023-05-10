f7ae4966879b3bebb34086ffbc378b1f18568172
Michael Hodges <Michael.Hodges1985@yahoo.com>
2023-05-10 18:12:19 -0500

    getting ready to deploy

--- server/server.js
+++ server/server.js
@@ -26,13 +26,13 @@ connectDB();
 const startServer = async () => {
   await server.start();
   
-  // if (process.env.NODE_ENV === 'production') {
-  //   app.use(express.static(path.join(__dirname, '../client/build')));
-  // }
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
 
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  // });
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 
   app.use(
     cors(),

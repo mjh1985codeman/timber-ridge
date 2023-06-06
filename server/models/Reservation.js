dd0b6585acee9778d78ea9eed7737bb0f11e4692
Michael Hodges <Michael.Hodges1985@yahoo.com>
2023-06-06 13:59:20 -0500

    added some updates to the reservation model to prep for payment flow implementation

--- server/models/Reservation.js
+++ server/models/Reservation.js
@@ -41,21 +41,27 @@ const ReservationMongooseSchema = new mongoose.Schema({
         },
         required: true,
     },
-    downPaymentPaid: {
-        type: Boolean,
-        required: true
-    },
     totalPrice: {
         type: Number,
         required: true
     },
+    downPaymentAmountRequired: {
+        type: Number,
+    },
+    paymentAmountCollected: {
+        type: Number,
+        default: 0.00
+    },
+    downPaymentPaid: {
+        type: Boolean,
+        default: false,
+    },
     balance: {
         type: Number,
-        required: true
     },
     paidInFull: {
         type: Boolean,
-        required: true
+        default: false,
     },
     property: {
         type: mongoose.Schema.Types.ObjectId,
@@ -69,7 +75,47 @@ const ReservationMongooseSchema = new mongoose.Schema({
     },
     cancelled: {
         type: mongoose.Schema.Types.Boolean,
+        default: false
+    },
+    dateCancelled: {
+        type: mongoose.Schema.Types.Date,
+        set: function(value) {
+         if(value !== null || "" || undefined) {
+             const date = new Date(value);
+             const easternDate = new Date(date.toLocaleString('en-US', { timeZone: 'America/New_York' }));
+             easternDate.setUTCHours(5);
+             easternDate.setUTCMinutes(5);
+             easternDate.setUTCSeconds(5);
+             easternDate.setUTCMilliseconds(5);
+             return easternDate;
+         } else {
+            return null
+         }
+        },
+        required: false,
     }
+});
+
+//defining a pre save hook that sets certain values of the reservation based on the payment history for the reservation.
+ReservationMongooseSchema.pre('save', function(next) {
+    const instance = this;
+    //calculate the down payment amount required.
+    instance.downPaymentAmountRequired = instance.totalPrice * .50;
+    //check to see if the paymentAmountCollected is greater or equal to the downPaymentAmountRequired
+    //If it is set the downPaymentPaid boolean to 'true'.  
+    if(instance.paymentAmountCollected >= instance.downPaymentAmountRequired) {
+        instance.downPaymentPaid = true;
+    //other wise we'll leave it at the default value of 'false'.
+    }
+    //calculate the balance based on the totalPrice and the paymentAmountCollected
+    instance.balance = instance.totalPrice - instance.paymentAmountCollected
+    //If the balance is less than or equal to 0.00 set the paidInFull boolean to true.
+    if(instance.balance <= 0.00) {
+        instance.paidInFull = true;
+        //otherwise we'll leave it at the default value which is false. 
+    } 
+    //just do the next function in the callStack (ie: save the data);
+    next();
 });
 
 module.exports = mongoose.model('Reservation', ReservationMongooseSchema);
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,13 +7,16 @@ type Reservation {
   _id: ID!
   beginDate: String
   endDate: String
-  downPaymentPaid: Boolean
   totalPrice: Float
+  downPaymentAmountRequired: Float
+  paymentAmountCollected: Float
+  downPaymentPaid: Boolean
   balance: Float
   paidInFull: Boolean
-  cancelled: Boolean
   property: Property
   customer: User
+  cancelled: Boolean
+  dateCancelled: String
 }
 
 type Property {
@@ -69,7 +72,20 @@ type Query {
 type Mutation {
   addProperty(name: String, reserved: Boolean, reserveCost: Float, addressSt: String, city: String, state: String, zip: Int, readyToReserve: Boolean, available: Boolean): Property
   addUser(firstName: String!, lastName: String!, phone: String, email: String!, role: String, password: String!): Auth
-  addReservation(beginDate: String, endDate: String, downPaymentPaid: Boolean, totalPrice: Float, balance: Float, paidInFull: Boolean, cancelled: Boolean, property: ID, customer: ID): Reservation
+  addReservation(
+    beginDate: String,
+    endDate: String,
+    totalPrice: Float,
+    downPaymentAmountRequired: Float,
+    paymentAmountCollected: Float,
+    downPaymentPaid: Boolean,
+    balance: Float,
+    paidInFull: Boolean,
+    property: ID,
+    customer: ID,
+    cancelled: Boolean,
+    dateCancelled: String
+  ): Reservation
   deleteReservation(_id: ID!):Reservation
   login(email: String!, password: String!): Auth
   sendReservationEmailConfirmation(emailInput: ReservationEmailInput): String
